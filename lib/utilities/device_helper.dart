93e798b93f6e1b7278a0b67a80f859a19c9a4a8d
Ng Jun Xiang <junxiang.ng.2016@smu.edu.sg>
2020-03-26 21:34:24 +0800

    Helper file to retrieve device info

---
+++ lib/utilities/device_helper.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:device_info/device_info.dart';
+
+class DeviceHelper {
+    static Future<List<String>> getDeviceDetails() async {
+        String deviceName;
+        String deviceVersion;
+        String identifier;
+        final DeviceInfoPlugin deviceInfoPlugin = new DeviceInfoPlugin();
+        try {
+            if (Platform.isAndroid) {
+                var build = await deviceInfoPlugin.androidInfo;
+                deviceName = build.model;
+                deviceVersion = build.version.toString();
+                identifier = build.androidId;  //UUID for Android
+            } else if (Platform.isIOS) {
+                var data = await deviceInfoPlugin.iosInfo;
+                deviceName = data.name;
+                deviceVersion = data.systemVersion;
+                identifier = data.identifierForVendor;  //UUID for iOS
+            }
+        } on PlatformException {
+            print('Failed to get platform version');
+        }
+
+//if (!mounted) return;
+        return [deviceName, deviceVersion, identifier];
+    }
+}

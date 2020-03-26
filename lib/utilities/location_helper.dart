0e197f46a6a832266e0387690f489fc6c8f9b538
Ng Jun Xiang <junxiang.ng.2016@smu.edu.sg>
2020-03-26 21:34:05 +0800

    Worked on location helper

--- lib/utilities/location_helper.dart
+++ lib/utilities/location_helper.dart
@@ -1,9 +1,49 @@
-import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
+import 'package:sp_annotations_app/models/geolocation.dart';
 
-class GeolocatorHelper {
+class LocationHelper {
+  static final Location location = Location();
+
   // once a second
-  static Future<Position> get currentLocation async {
-    return await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  static Future<bool> init() async {
+    return checkForPermissions();
+  }
+
+  static Future<bool> checkForPermissions() async {
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return false;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.DENIED) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.GRANTED) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  static Future<Geolocation> getGeolocation() async {
+    LocationData locationData = await location.getLocation();
+
+    return Geolocation(
+      '123',
+      DateTime.fromMillisecondsSinceEpoch(locationData.time.toInt()),
+      '123',
+      locationData.latitude,
+      locationData.longitude,
+      locationData.accuracy,
+      locationData.altitude,
+      locationData.speed,
+    );
   }
 }
